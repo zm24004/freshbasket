0d5129c76711bc635f5deaa8aed70a2beeeffa34
RM24004 <167376455+RM24004@users.noreply.github.com>
2026-04-09 22:46:40 -0600

    Feat: Swagger y Api
    Agregada la dependecia de Swagger para los docs de la api

--- backend/src/main/java/com/group1/proyect/freshbasket/FreshbasketApplication.java
+++ backend/src/main/java/com/group1/proyect/freshbasket/FreshbasketApplication.java
@@ -1,11 +1,20 @@
 package com.group1.proyect.freshbasket;
-
+//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPiDefinition(
+		info = @Info(
+			title = "Inventario de tienda",
+			version = "1.0",
+			description = "Api para gestion de inventatio",
+			contact = @Contact(name = "victor", email = "victor@mail.com") 
+		)
+	)
 @SpringBootApplication
 public class FreshbasketApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FreshbasketApplication.class, args);
 	}
---
+++ backend/src/main/java/com/group1/proyect/freshbasket/OpenAPiDefinition.java
@@ -0,0 +1,9 @@
+package com.group1.proyect.freshbasket;
+
+import io.swagger.v3.oas.annotations.info.Info;
+
+public @interface OpenAPiDefinition {
+
+    Info info();
+
+}
